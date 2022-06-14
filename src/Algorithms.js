f27766920089c484cee55043b4664a8d1dc21fb6
Sidhant <sidhant26sood@gmail.com>
2022-06-14 16:45:35 +0530

    Implemented some sorting

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -1,4 +1,5 @@
-export const bubbleSort = arr =>{
+export const bubbleSort = (arr,colourInt,arrInt) =>{
+  let clr=colourInt[colourInt.length-1].slice()
     for(let i=0;i<arr.length;i++)
     {
         for(let j=0;j<arr.length-i-1;j++)
@@ -7,11 +8,71 @@ export const bubbleSort = arr =>{
           {
             [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
           }
+          arrInt.push(arr.slice())
+          clr[j]=1
+          clr[j+1]=1
+          colourInt.push(clr.slice())
+          clr[j]=0
+          clr[j+1]=0
         }
+        clr[arrInt.length-1-i]=2
+        arrInt.push(arr.slice())
+        colourInt.push(clr.slice()) 
       }
-      return arr
+      colourInt[colourInt.length-1]=new Array(arr.length).fill(2)
 }
 
-export const selectionSort = arr =>{
-  
+function merge(arr,l,h,animations,temp)
+{
+  const mid=Math.floor((l+h)/2)
+  let i=l,j=l,k=mid+1
+  while(i<=mid && k<=h)
+  {
+    animations.push([i,k])
+    animations.push([i,k])
+    if(temp[i]<=temp[k])
+    {
+      animations.push([j,temp[i]])
+      arr[j++]=temp[i++]
+    }
+    else
+    {
+      animations.push([j,temp[k]])
+      arr[j++]=temp[k++]
+    }
+  }
+  while(i<=mid)
+  {
+    animations.push([i,i])
+    animations.push([i,i])
+    animations.push([j,temp[i]])
+    arr[j++]=temp[i++]
+  }
+  while(j<=h)
+  {
+    animations.push([k,k])
+    animations.push([k,k])
+    animations.push([k,temp[j]])
+    arr[j++]=temp[k++]
+  }
+}
+
+function mergeSort(arr,l,h,temp,animations)
+{
+  if(l==h)
+  return
+  const mid=Math.floor((l+h)/2)
+  mergeSort(temp,l,mid,arr,animations)
+  mergeSort(temp,mid+1,h,arr,animations)
+  merge(arr,l,h,animations,temp)
+}
+
+export function mergeAnimations(arr)
+{
+  const animations=[]
+  if(arr.length<=1)
+  return arr
+  const temp=arr.slice()
+  mergeSort(arr,0,arr.length-1,temp,animations)
+  return animations
 }
--- src/components/Visualiser.jsx
+++ src/components/Visualiser.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./Visualiser.css"
 import * as algo from "../Algorithms"
 
+const primary="aqua",sec="red",animSpeed=1
+
 export default function Visualiser()
 {
     const [arr,setArr]=React.useState([])
     function getArray()
     {
         const a=[]
-        for(let i=0;i<240;i++)
+        for(let i=0;i<280;i++)
         a.push(generateRandom(5,732))
         setArr(a)
     }
@@ -28,10 +30,39 @@ export default function Visualiser()
         console.log(arr)
     }
 
+    function mergeSort()
+    {
+        const animations=algo.mergeAnimations(arr)
+        for(let i=0;i<animations.length;i++)
+        {
+            const bars=document.getElementsByClassName("arr-viz")
+            const colourChange=i%3!==2
+            if(colourChange)
+            {
+                const [firstInd,secondInd] = animations[i]
+                const firstBar = bars[firstInd].style
+                const secondBar = bars[secondInd].style
+                const colour=i%3 === 0 ? primary : sec
+                setTimeout(() => {
+                    firstBar.backgroundColor=colour
+                    secondBar.backgroundColor=colour
+                }, i*animSpeed);
+            }
+            else
+            {
+                setTimeout(() => {
+                    const [firstInd,newHeight] = animations[i]
+                    const firstBar = bars[firstInd].style
+                    firstBar.height=`${newHeight}px`
+                }, i*animSpeed);
+            }
+        }
+    }
+
     return(
         <>
             <button onClick={getArray} className="btn"> Get a new array</button>
-            <button onClick={bubbleSort} className="btn">bs</button>
+            <button onClick={mergeSort} className="btn">ms</button>
             <br /><br />
             <div className="arr-flex">
                 {arr.map((value,ind)=>(
