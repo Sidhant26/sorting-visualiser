edfe3383503d26d4cb9a451c1995f7cfecea5017
Sidhant <sidhant26sood@gmail.com>
2022-06-14 13:26:33 +0530

    Fixed sorting bugs

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -1,4 +1,4 @@
-export const bubbleSort= arr =>{
+export const bubbleSort = arr =>{
     for(let i=0;i<arr.length;i++)
     {
         for(let j=0;j<arr.length-i-1;j++)
@@ -9,4 +9,9 @@ export const bubbleSort= arr =>{
           }
         }
       }
+      return arr
+}
+
+export const selectionSort = arr =>{
+  
 }
--- src/components/Visualiser.jsx
+++ src/components/Visualiser.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Visualiser.css"
-import "../Algorithms"
+import * as algo from "../Algorithms"
 
 export default function Visualiser()
 {
@@ -8,8 +8,8 @@ export default function Visualiser()
     function getArray()
     {
         const a=[]
-        for(let i=0;i<220;i++)
-        a.push(generateRandom(5,734))
+        for(let i=0;i<240;i++)
+        a.push(generateRandom(5,732))
         setArr(a)
     }
 
@@ -22,13 +22,21 @@ export default function Visualiser()
         getArray();
     },[0])
 
+    function bubbleSort()
+    {
+        algo.bubbleSort(arr)
+        console.log(arr)
+    }
+
     return(
         <>
             <button onClick={getArray} className="btn"> Get a new array</button>
+            <button onClick={bubbleSort} className="btn">bs</button>
             <br /><br />
             <div className="arr-flex">
                 {arr.map((value,ind)=>(
-                    <div className="arr-viz" key={ind} style={{height:`${value}px`}}>
+                    <div className="arr-viz" key={ind} 
+                    style={{height:`${value}px`, width: `${arr.length<50 ? 8 : 2}px` }}>
                     </div>
                 ))}
             </div>
